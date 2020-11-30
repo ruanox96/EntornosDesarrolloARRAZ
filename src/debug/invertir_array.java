78315e768214cb63fa8a3811cb7676c276deff2c
ruanox96 <72544074+ruanox96@users.noreply.github.com>
2020-11-30 11:03:37 +0100

    lelele

--- src/debug/invertir_array.java
+++ src/debug/invertir_array.java
@@ -12,7 +12,7 @@ public class invertir_array {
 		int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 		int aux = 0;
 		// imprimimos array para comprobar
-		int longitud = array.length;
+		int longitud = array.length -1;
 		for (int i = 0; i <= longitud; i++)
 			System.out.print(array[i] + " ");
 		// invertimos array recorriéndolo hasta la mitad e intercambiando valores
@@ -21,13 +21,13 @@ public class invertir_array {
 			aux = array[longitud - i];
 			// como ya tenemos guardada la posición longiutd-i-1, ya podemos guardar en esa
 			// posición un valor
-			array[longitud - i - 1] = array[i];
+			array[longitud - i] = array[i];
 			// guardamos en las posiciones iniciales el valor de aux que ya esta guardado
 			array[i] = aux;
 		}
 		// comprobamos que se ha invertido el array
 		System.out.println();
-		for (int i = 0; i < longitud; i++)
+		for (int i = 0; i <= longitud; i++)
 			System.out.print(array[i] + " ");
 	}
 }
