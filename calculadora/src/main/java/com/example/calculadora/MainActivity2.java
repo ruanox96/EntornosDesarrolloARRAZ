2588053b3caa53c2b31769af309018b4d1981ab1
Antonio Ruiz-Ruano <72544074+ruanox96@users.noreply.github.com>
2021-05-07 00:03:27 +0200

    Add files via upload

---
+++ calculadora/src/main/java/com/example/calculadora/MainActivity2.java
@@ -0,0 +1,24 @@
+package com.example.calculadora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView TextView2 = findViewById(R.id.textView2);
+        TextView2.setText(message);
+    }
+
+
+}
